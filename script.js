437656506f70ed80737749d6349fe20e30ee9c3d
Pedroangelv <grijalbaangel393@gmail.com>
2025-03-09 16:27:40 -0500

    firts steps

---
+++ script.js
@@ -0,0 +1,11 @@
+let n1 = document.getElementById("n1").value;
+let n2 = document.getElementById("n2").value;
+function salida() {
+  let m = document.createElement("div");
+  m.classList.add("salida")
+  m.innerHTML = `
+  <h1>adaw</h1>
+
+  `;
+  return m;
+}
