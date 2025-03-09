01abdee1fd520c88b14e648af639b9a40235f316
Pedroangelv <grijalbaangel393@gmail.com>
2025-03-09 18:13:25 -0500

    sum fun

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-let n1 = document.getElementById("n1").value;
-let n2 = document.getElementById("n2").value;
-function salida() {
-  let m = document.createElement("div");
-  m.classList.add("salida")
-  m.innerHTML = `
-  <h1>adaw</h1>
+let y1 = document.getElementById("n1").value;
+let x2 = document.getElementById("n2").value;
 
-  `;
-  return m;
+n1 = parseInt(y1);
+n2 = parseInt(x2);
+
+
+
+function sum() {
+  let n = document.createElement("div");
+  n.innerHTML = ""
+  n.innerHTML = `
+  <h1>${n1+n2}</h1>
+  `
+  document.getElementById("Resultado").appendChild(n)
 }
