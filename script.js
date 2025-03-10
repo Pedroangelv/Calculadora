5a9f72784ea6c2c7e83a8e29aee81163ee2e0161
Pedroangelv <grijalbaangel393@gmail.com>
2025-03-09 20:45:37 -0500

    sum feature complete

--- script.js
+++ script.js
@@ -2,15 +2,14 @@
 
 
 function sum() {
+  
+  
+
   let y1 = document.getElementById("n1").value;
   let x2 = document.getElementById("n2").value;
 
   n1 = parseInt(y1);
   n2 = parseInt(x2);
-  let n = document.createElement("div");
-  n.innerHTML = ``
-  n.innerHTML = `
-  <h1>${n1+n2}</h1>
-  `
-  document.getElementById("Resultado").appendChild(n)
+  let resultadoDiv = document.getElementById("Resultado");
+  resultadoDiv.innerHTML = `<h1>${n1 + n2}</h1>`;
 }
