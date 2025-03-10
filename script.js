d586816189a420dc67b96363a39b6c52389c7ead
Pedroangelv <grijalbaangel393@gmail.com>
2025-03-09 20:11:53 -0500

    sum realized

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let y1 = document.getElementById("n1").value;
-let x2 = document.getElementById("n2").value;
-
-n1 = parseInt(y1);
-n2 = parseInt(x2);
 
 
 
 function sum() {
+  let y1 = document.getElementById("n1").value;
+  let x2 = document.getElementById("n2").value;
+
+  n1 = parseInt(y1);
+  n2 = parseInt(x2);
   let n = document.createElement("div");
-  n.innerHTML = ""
+  n.innerHTML = ``
   n.innerHTML = `
   <h1>${n1+n2}</h1>
   `
